f5078717266049d42f8989413985cafbc123e9bc
Adam Jonas <adam.jonas@paddypowerbetfair.com>
2021-06-22 09:21:37 +0100

    Initial commit

---
+++ session-3-notes.md
@@ -0,0 +1,6 @@
+# Java Course - Session 3 
+
+## Primitive Types
+
+boolean, byte, char, short, int, long, float, double
+
---
+++ src/session3/PrimitiveTypes/Main.java
@@ -0,0 +1,17 @@
+package session3.PrimitiveTypes;
+
+public class Main {
+    public static void main(String[] args) {
+        int minimumIntValue = Integer.MIN_VALUE;
+        int maximumIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Maximum Value = " + maximumIntValue);
+        System.out.println("Integer Minimum Value = " + minimumIntValue);
+    }
+}
+
+// ## Primitive Types
+// boolean, byte, char, short, int, long, float, double
+
+// every primitive has a wrapper class
+// Primitive wrap

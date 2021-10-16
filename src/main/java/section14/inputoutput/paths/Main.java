ed45dbfb42bffe56bdca2b11a02db1e057c16fe0
Adam Jonas <adam.jonas@ppb.com>
2021-10-16 23:45:32 +0100

    File manipulation

--- src/main/java/section14/inputoutput/paths/Main.java
+++ src/main/java/section14/inputoutput/paths/Main.java
@@ -1,16 +1,22 @@
 package section14.inputoutput.paths;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileStore;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 
 public class Main {
     public static void main(String[] args) {
-         createFile();
+        fileStores();
     }
 
     private static void examplesWithFilePaths() {
@@ -96,5 +102,100 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void createDirectories() {
+        try {
+            Path dirToCreate = Paths.get("examples", "dir2/dir3/dir4/dir5/dir6");
+            Files.createDirectories(dirToCreate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void getFileAttributes() {
+        try {
+            Path filePath = Paths.get("examples", "dir1", "file1.txt");
+            long size = Files.size(filePath);
+            FileTime lastModified = Files.getLastModifiedTime(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified at = " + lastModified);
+
+            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attributes.size());
+            System.out.println("Last modified at = " + attributes.lastModifiedTime());
+            System.out.println("Created at = " + attributes.creationTime());
+            System.out.println("Is directory = " + attributes.isDirectory());
+            System.out.println("Is regular file = " + attributes.isRegularFile());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readExistingDirectoryContents() {
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            public boolean accept(Path path) throws IOException {
+                return Files.isRegularFile(path);
+            }
+        };
+
+        Path directory = FileSystems.getDefault().getPath("examples/dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void filterDirectoriesWithLambda() {
+        DirectoryStream.Filter<Path> filter = Files::isRegularFile;
+
+        Path directory = FileSystems.getDefault().getPath("examples/dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void osAgnosticSeparator() {
+        String separator = File.separator;
+        System.out.println(separator);
+
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+    }
+
+    private static void createTemporaryFile() {
+        try {
+            Path tempFile = Files.createTempFile("myapp", ".appext");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+//            Temporary file path = /var/folders/x_/0655n6d1587_ctpk__mbtsvsn80rmp/T/myapp11807715093333568973.appext
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileStores() {
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for (FileStore fileStore : stores) {
+            System.out.println("Volume name = " + fileStore.name());
+            System.out.println("File store = " + fileStore);
+        }
+
+        System.out.println("**********************");
+        Iterable<Path> rootPaths = FileSystems.getDefault().getRootDirectories();
+        for (Path path : rootPaths) {
+            System.out.println("Root path = " + path);
+        }
+    }
+
+    private static void walkFileTree() {
+        
     }
 }
